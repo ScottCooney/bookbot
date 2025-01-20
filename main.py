3c59e3f3b6a7ec811aea9a5255a89a0fd9e97689
ScottCooney <snj8@wildcats.unh.edu>
2025-01-20 17:42:25 -0500

    added report function

--- main.py
+++ main.py
@@ -2,8 +2,26 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(char_count(get_book_text(book_path)))
+    num_chars = char_count(text)
+    report(num_words,num_chars)
     
+
+def report(wordcount,chars):
+    alpha = []
+    print(f"--- Begin report of books/frankenstein.txt ---")
+    print(f"{wordcount} words found in the document")
+    for char in chars:
+        if char.isalpha() == True:
+            alpha.append({char : chars[char]})
+            #print(f"{char} has {chars[char]}")
+    def sort_on(item):
+        return list(item.values())[0]
+    alpha.sort(reverse=True, key=sort_on)
+    for i in range (len(alpha)):
+        for char, count in alpha[i].items(): 
+            print(f"The '{char}' character was found {count} times")
+
+
 
 def char_count(input):
     characters = {}
